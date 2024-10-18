f2ccaa3542cd10c373c6454c82ec7f6f8063fa02
Amsilva3 <amsilva3@gmail.com>
2024-10-18 12:31:32 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'prettier', 'universe/native','universe'],
+  plugins: ['prettier', 'react', 'react-native'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-use-before-define': ['error', { variables: false }],
+    'react/prop-types': ['error', { ignore: ['navigation', 'navigation.navigate'] }],
+  },
+};
---
+++ src/frases/frases.js
@@ -0,0 +1,93 @@
+const frases = [
+    'Posso todas as coisas naquele que me fortalece',
+
+    'Porque sou eu que conheço os planos que tenho para vocês, diz o Senhor, planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro',
+
+    'Ninguém o despreze por você ser jovem, mas seja um exemplo para os fiéis na palavra, no comportamento, no amor, na fé e na pureza',
+
+    'Como pode o jovem manter pura a sua conduta? Vivendo de acordo com a tua palavra',
+
+    'Como pode o jovem manter pura a sua conduta? Vivendo de acordo com a tua palavra',
+
+    'Confie no Senhor de todo o seu coração e não se apoie em seu próprio entendimento; reconheça o Senhor em todos os seus caminhos, e ele endireitará as suas veredas',
+
+    'Não to mandei eu? Esforça-te, e tem bom ânimo; não pasmes, nem te espantes; porque o Senhor, teu Deus, é contigo, por onde quer que andares',
+
+    'Porque Deus não nos deu espírito de covardia, mas de poder, de amor e de equilíbrio',
+
+    'E não vos conformeis com este mundo, mas transformai-vos pela renovação da vossa mente, para que experimenteis qual seja a boa, agradável e perfeita vontade de Deus',
+
+    'Mas aqueles que esperam no Senhor renovarão as suas forças. Voarão alto como águias; correrão e não ficarão exaustos, andarão e não se cansarão',
+
+    'Tudo quanto fizerdes, fazei-o de todo o coração, como para o Senhor e não para homens, cientes de que recebereis do Senhor a recompensa da herança. A Cristo, o Senhor, é que estais servindo',
+
+    'Todas as vossas coisas sejam feitas com amor',
+
+    'Filhinhos, vós sois de Deus e tendes vencido os falsos profetas, porque maior é aquele que está em vós do que aquele que está no mundo.',
+
+    'Mas buscai primeiro o Reino de Deus, e a sua justiça, e todas estas coisas vos serão acrescentadas',
+
+    'Confia ao Senhor as tuas obras, e teus pensamentos serão estabelecidos.',
+
+    'O coração do homem traça o seu caminho, mas o Senhor lhe dirige os passos.',
+
+    'Tão somente sê forte e mui corajoso para teres o cuidado de fazer segundo toda a lei que meu servo Moisés te ordenou; dela não te desvies, nem para a direita nem para a esquerda, para que sejas bem-sucedido por onde quer que andares.',
+
+    'Portanto, meus amados irmãos, sede firmes e constantes, sempre abundantes na obra do Senhor, sabendo que, no Senhor, o vosso trabalho não é vão.',
+
+    'Nada façais por contenda ou por vanglória, mas por humildade, cada um considerando os outros superiores a si mesmo. Não atente cada um para o que é propriamente seu, mas cada qual também para o que é dos outros.',
+
+    'Onde não há conselho, frustram-se os projetos, mas, com a multidão de conselheiros, se confirmam.',
+
+    'Não sejais vagarosos no cuidado; sede fervorosos no espírito, servindo ao Senhor.',
+
+    'Tendo o cuidado de fazer o que é honesto, não só perante o Senhor, mas também diante dos homens.',
+    'Os projetos fracassam pela falta de conselhos, mas são bem-sucedidos quando há muitos conselheiros.',
+
+    'E, se algum de vós tem falta de sabedoria, peça-a a Deus, que a todos dá liberalmente e não o lança em rosto; e ser-lhe-á dada.',
+
+    'Lembrai-vos dos vossos guias, que vos pregaram a palavra de Deus, e, atentando para o êxito da sua carreira, imitai a fé que tiveram.',
+
+    'Não será assim entre vós; mas todo aquele que quiser entre vós fazer-se grande, que seja vosso serviçal.',
+
+    'Fiel é a palavra: se alguém aspira ao episcopado, excelente obra almeja.',
+    'Tudo posso naquele que me fortalece.',
+
+    'Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel.',
+
+    'Deus é o nosso refúgio e fortaleza, socorro bem presente nas tribulações.',
+
+    'Sabemos que todas as coisas cooperam para o bem daqueles que amam a Deus, daqueles que são chamados segundo o seu propósito.',
+
+    'Confia no Senhor de todo o teu coração e não te estribes no teu próprio entendimento. Reconhece-o em todos os teus caminhos, e ele endireitará as tuas veredas.',
+
+    'Porque Deus não nos deu espírito de covardia, mas de poder, de amor e de moderação.',
+
+    'Não to mandei eu? Esforça-te e tem bom ânimo; não pasmes, nem te espantes, porque o Senhor, teu Deus, é contigo, por onde quer que andares.',
+    'O Senhor é o meu pastor, nada me faltará.',
+
+    'Vinde a mim, todos os que estais cansados e oprimidos, e eu vos aliviarei.',
+
+    'Deleita-te também no Senhor, e ele te concederá o que deseja o teu coração.',
+
+    'Porque andamos por fé, e não pelo que vemos.',
+
+    'Elevo os olhos para os montes; de onde me vem o socorro? O meu socorro vem do Senhor, que fez o céu e a terra.',
+
+    'Prossigo para o alvo, para o prêmio da soberana vocação de Deus em Cristo Jesus.',
+
+    'Por que você está assim tão triste, ó minha alma? Por que está assim tão perturbada dentro de mim? Ponha a sua esperança em Deus! Pois ainda o louvarei; ele é o meu Salvador e o meu Deus.',
+
+    'Espere no Senhor. Seja forte! Coragem! Espere no Senhor.',
+
+    'Porque sou eu que conheço os planos que tenho para vocês, diz o Senhor, planos de fazê-los prosperar e não de causar dano, planos de dar a vocês esperança e um futuro.',
+
+    'Espere no Senhor. Seja forte! Coragem! Espere no Senhor.',
+
+    'A minha porção é o Senhor, diz a minha alma; portanto, esperarei nele.',
+
+    'Guardemos firme a confissão da esperança, sem vacilar, pois quem fez a promessa é fiel.',
+
+    'E a esperança não traz confusão, porquanto o amor de Deus está derramado em nosso coração pelo Espírito Santo que nos foi dado.',
+];
+export default frases;
